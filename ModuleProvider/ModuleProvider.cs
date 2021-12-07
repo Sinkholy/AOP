41fe505aa80488d15c93fc2fc9d7a1b303385948
Sinkhole <Sinkholy@gmail.com>
2021-12-07 21:23:05 +0300

    [Development][Базовая реализация провайдера модулей]

--- ModuleProvider/ModuleProvider.cs
+++ ModuleProvider/ModuleProvider.cs
@@ -6,54 +6,54 @@ namespace AOP.ModuleProvider
 {
 	public class ModuleProvider
 	{
+		const string DirectorySeparator = @"\";
+
 		public ModuleProvidingResult GetModuleFromPath(string path, string moduleName)
 		{
-			// Если path == null или пустой
-				// Вернуть отрицательный результат с параметрами:
-					// errorDesc - path == null или пустой
-					// Exception - ArgumentException
-			// Если moduleName == null или пустой
-				// Вернуть отрицательный результат с параметрами:
-					// errorDesc - передан невалидный путь
-					// Exception - ArgumentException
-			// Объявить переменную типа string fullPath
-			// Объявить переменную типа ModuleDefenition result
-			// Проверить содержит ли переменная path расширение файла
-			// Если содержит
-				// Вычленить имя файла, указанного в path
-				// Сравнить полученое имя файла с moduleName
-				// Если они совпадают
-					// Присвоить значение path в переменную fullPath
-			// Иначе
-				// Проверить содержит ли переменная path сепаратора (\) в окончании
-					// Если не содержит
-						// Добавить сепаратор к окончанию
-				// Проверить содержит ли переменная moduleName расширение файла
-					// Если не содержит
-						// Вернуть отрицательный результат с параметрами:
-							// errorDesc - передано невалидное название модуля
-							// Exception - ArgumentException
-					// Иначе
-						// Скомбинировать path и moduleName и присвоисть результат в fullPath
+			if (string.IsNullOrWhiteSpace(path))
+			{
+				string errorDesc = "";
+				ArgumentNullException ex = new (nameof(path), errorDesc);
+				return ModuleProvidingResult.Failed(errorDesc, ex);
+			}
+			if (string.IsNullOrWhiteSpace(moduleName))
+			{
+				string errorDesc = "";
+				ArgumentNullException ex = new (nameof(moduleName), errorDesc);
+				return ModuleProvidingResult.Failed(errorDesc, ex);
+			}
+			if (PathContainsFileExtension())
+			{
+				string errorDesc = "";
+				ArgumentException ex = new (errorDesc, nameof(path));
+				return ModuleProvidingResult.Failed(errorDesc, ex);
+			}
+			if (!PathEndsWithSeparator())
+			{
+				path += DirectorySeparator;
+			}
+
+			ModuleDefinition result;
+			string fullPath = path + moduleName;
+
 			try
 			{
-				// Получить ModuleDefenition используя fullPath
+				result = ModuleDefinition.ReadModule(fullPath);
 			}
-			catch // Исключение ввода-вывода
+			catch(Exception ex)
 			{
-				// Вернуть отрицательный результат с параметрами:
-					// errorDesc - невозможно получить доступ к файлу модуля
-					// Exception - ArgumentException
+				return ModuleProvidingResult.Failed(ex.Message, ex);
 			}
-			catch // Другие исключения
+			return ModuleProvidingResult.Successfull(result);
+
+			bool PathContainsFileExtension()
 			{
-				// Вернуть отрицательный результат с параметрами:
-					// errorDesc - ошибка про чтении модуля
-					// Exception - ArgumentException
+				return false;
 			}
-			// Вернуть положительный результат с параметрами:
-				// Result = result
-			return null;
+			bool PathEndsWithSeparator()
+			{
+				return true;
+			}
 		}
 
 		public class ModuleProvidingResult
@@ -68,7 +68,7 @@ namespace AOP.ModuleProvider
 			internal static ModuleProvidingResult Successfull(ModuleDefinition result) => new(null, null, result);
 			internal static ModuleProvidingResult Failed(string errorDesc, Exception exception = null) => new(errorDesc, exception, default);
 
-			public bool IsSuccessFull => ErrorDescription != null;
+			public bool IsSuccessFull => ErrorDescription == null;
 			public string ErrorDescription { get; private set; }
 			public Exception Exception { get; private set; }
 			public ModuleDefinition Result { get; private set; }
