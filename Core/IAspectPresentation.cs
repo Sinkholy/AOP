f18d2bcf4c798fefbc19b7537ca2d57e5deffe5f
Sinkhole <Sinkholy@gmail.com>
2021-12-10 23:22:21 +0300

    [Development][Добавил проект ядра и объявил базовые контракты аспета\совета\среза\точки внедрения][Проект ядра будет связывать все остальные проекты в единую цепочку выполнения. Помимо внешних так же объявил внутренние контракты необходимые в процессе работы системы]

---
+++ Core/IAspectPresentation.cs
@@ -0,0 +1,18 @@
+﻿namespace Core
+{
+	public interface IAspectPresentation
+	{
+		AspectBase Aspect { get; }
+		IPointcutPresentation[] Pointcuts { get; }
+
+		public interface IPointcutPresentation
+		{
+			AspectBase.PointcutBase Pointcut { get; }
+			IAdvicePresentationBase[] Advices { get; }
+		}
+		public interface IAdvicePresentationBase
+		{
+			AspectBase.AdviceBase Advice { get; }
+		}
+	}
+}
---
+++ Core/IJoinPoint.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Reflection;
+
+namespace Core
+{
+	public interface IJoinPoint
+	{
+		IContext Context { get; }
+		IMetadata Metadata { get; }
+
+		public interface IContext
+		{
+			IArgument[] Arguments { get; }
+			IVariable[] Variables { get; }
+
+			public interface IEntity
+			{
+				string Name { get; }
+				Type Type { get; }
+				object Value { get; }
+			}
+			public interface IVariable : IEntity
+			{
+			}
+			public interface IArgument : IEntity
+			{
+			}
+		}
+		public interface IMetadata
+		{
+			string Name { get; }
+			Type JoinPointType { get; }
+			System.Type DeclaredIn { get; }
+			IParameter[] Parameters { get; }
+			bool ContainsParameters => Parameters != null && Parameters.Length != 0;
+			System.Type ReturnType { get; }
+			MemberInfo MemberInfo { get; }
+
+			interface IParameter
+			{
+				// TODO: нужен ли вообще этот интерфейс?
+				// Сейчас я использую его как некий shortcut для ParameterInfo
+				// т.к. Name и Type просто обращаются к ParameterInfo
+				// В принципе это выглядит более "user friendly"
+				// Но это так же может усложнять структуру
+				// Нужно решить:
+				// - Оставить всё какесть
+				// - Избавиться от интерфейса и использовать ParameterInfo напрямую
+				// - Избавиться от ParameterInfo и добавлять сущности по необходимости
+				// Так же всё вышесказанное вполне относится к IMetadata ^^^
+				string Name { get; }
+				System.Type Type { get; }
+				ParameterInfo ParameterInfo { get; }
+			}
+			enum Type : byte
+			{
+				Getter,
+				Setter,
+				Method,
+				Constructor
+				// TODO: etc
+			}
+		}
+	}
+}
